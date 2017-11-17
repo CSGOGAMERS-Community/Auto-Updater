a19d3a968281a8deb6eb00600196172bda71e15e
Kyle <h673321480@qq.com>
2017-11-17 11:15:53 +0800

    fix web error

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php 
 
-if(!isset($_GET['plugin']){
+if(!isset($_GET['plugin'])){
     echo 'Plugin ERROR';
-    die(404)
-}
-
-$path = __DIR__ . "/" . $_GET['plugin'] . "/Raw/" . $_GET['plugin'] . ".smx";
-
-if(!file_exists($path)){
-    echo 'Plugin NOT exists!';
-    die(404);
-}
-
-if(!isset($_GET['md5'])){
-    echo 'MD5 ERROR!';
     die(404);
 }
 
@@ -22,10 +10,22 @@ if(!isset($_GET['file'])){
     die(404);
 }
 
+if(!isset($_GET['md5'])){
+    echo 'MD5 ERROR!';
+    die(404);
+}
+
+$path = __DIR__ . "/" . $_GET['plugin'] . "/Raw/" . $_GET['file'];
+
+if(!file_exists($path)){
+    echo 'Plugin NOT exists!';
+    die(404);
+}
+
 $md5 = md5_file($path);
 
 if(strcmp($md5, $_GET['md5']) == 0){
-    echo 'Plugin '. $_GET['plugin'] . ' is up to date';
+    echo 'Plugin ' . $_GET['plugin'] . ' is up to date';
     die(200);
 }
 
